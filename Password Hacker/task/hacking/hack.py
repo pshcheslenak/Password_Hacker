bb0e487a72d097e4eadcd57506b3821104d091ac
Petr Shcheslenak <sheslenok@bk.ru>
2021-09-24 13:24:01 +0300

    Added using of files with dictionary of typical passwords

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,4 +1,3 @@
-import string
 import sys
 import socket
 import itertools
@@ -6,24 +5,33 @@ import itertools
 args = sys.argv
 address = (args[1], int(args[2]))
 
-chars = list(string.ascii_lowercase) + [str(d) for d in range(10)]
+passwords = open('passwords.txt', 'r')
 
-with socket.socket() as client_socket:
-    client_socket.connect(address)
+client_socket = socket.socket()
+client_socket.connect(address)
 
-    list_of_iters = []
-    flag = True
-    while flag:
-        list_of_iters.append(chars)
-        my_iter = itertools.product(*list_of_iters)
-        for pwd in my_iter:
-            message_bytes = ''.join(pwd).encode()
-            client_socket.send(message_bytes)
+for line in passwords:
 
-            response = client_socket.recv(1024).decode()
-            if response == "Connection success!":
-                print(''.join(pwd))
-                flag = False
-                break
-            elif response == "Too many attempts":
-                exit(0)
+    line = line.strip('\n')
+    chars = [[c, c.upper()] if not c.isdigit() else c for c in line]
+    my_iter = itertools.product(*chars)
+
+    for pwd in my_iter:
+        message_bytes = ''.join(pwd).encode()
+        client_socket.send(message_bytes)
+
+        response = client_socket.recv(1024).decode()
+
+        if response == "Connection success!":
+            print(''.join(pwd))
+            break
+        elif response == "Too many attempts":
+            break
+
+    else:
+        continue
+
+    break
+
+passwords.close()
+client_socket.close()
